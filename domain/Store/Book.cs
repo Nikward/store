316253d845a018a61b891b5e09deb962220f7fe7
norvo <nik.ward@yandex.ru>
2024-05-01 16:49:19 +0300

    fix errors

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Store
 {
-    internal class Book
+    public class Book
     {
+        public int Id { get; }
+
+        public string Title { get; }
+
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
     }
 }
---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
