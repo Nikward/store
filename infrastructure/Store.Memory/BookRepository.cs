463eba1fc1eb6ff6415a2136630c98bc27da90fb
norvo <nik.ward@yandex.ru>
2024-05-01 17:10:02 +0300

    Добавить реализацию репозитория книг в памяти

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,10 +1,22 @@
-﻿namespace Store.Memory
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
 {
     public class BookRepository : IBookRepository
     {
+
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language"),
+        };
+
         public Book[] GetAllByTitle(string titlePart)
         {
-            throw new NotImplementedException();
+            return books.Where(book => book.Title.Contains(titlePart))
+                .ToArray();
         }
     }
 }
